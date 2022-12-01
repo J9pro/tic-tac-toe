8fb5c1df393fdbf51c58135661a6612a829b31e1
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2022-12-01 18:02:06 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// event listners for each cell
+
+//function to start the game, maybe ask for a name
+
+//maybe a function that "flips a coin" to choose who is going first
+
+//function to let the first player play
+
+//similar function to let the second player play
+
+// some function to see who's turn is it, maybe some variable which changes a boolean value each time someone play
+
+// function to check the status of the game and winner
+
+//function to restart the game
+
