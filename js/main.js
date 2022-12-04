04f5afb864bfc4f76425c4f6397551759577a657
Andres Gonzalez <106206361+andresgonzalezarbildi@users.noreply.github.com>
2022-12-04 18:54:44 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,3 +14,36 @@
 
 //function to restart the game
 
+let turn = 'X'
+document.querySelectorAll(".board div").forEach(element => {
+    element.addEventListener('click', () => {
+        if (element.textContent === "") {
+          element.textContent = turn;
+          turn === "O" ? (turn = "X") : (turn = "O");
+        }
+        return
+    })
+})
+
+
+
+// document.getElementById(".a1").addEventListener('click',playerX)
+
+function playerX(){
+//     let a1 = document.querySelector("#a1").textContent
+//     let a2 = document.querySelector("#a2").innerHTML
+//     let a3 = document.querySelector("#a3").innerHTML
+//     let b1 = document.querySelector("#b1").innerHTML
+//     let b2 = document.querySelector("#b2").innerHTML
+//     let b3 = document.querySelector("#b3").innerHTML
+//     let c2 = document.querySelector("#c2").innerHTML
+//     let c1 = document.querySelector("#c1").innerHTML
+//     let c3 = document.querySelector("#c3").innerHTML
+
+//         console.log("WIn")
+    
+//     console.log("check")
+    let winningConditions = [
+        ['1', '2', '3']
+    ]
+}
